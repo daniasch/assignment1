6f3c6fe004bcb5b3f82d7cf8b15c4ce397d0b939
daniasch <57307635+daniasch@users.noreply.github.com>
2019-11-03 01:20:25 -0500

    Update Greetings.java
    Added method called String time().

--- src/main/java/edu/harrisburgu/cisc525/app/Greetings.java
+++ src/main/java/edu/harrisburgu/cisc525/app/Greetings.java
@@ -3,12 +3,23 @@ package edu.harrisburgu.cisc525.app;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.lang.management.ManagementFactory;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Greetings class
  * 
  */
 public class Greetings {
+    
+    public String time(){
+       Calendar cal = Calendar.getInstance();
+       SimpleDateFormat dateTime = new SimpleDateFormat("dd:MM:yyyy HH:mm:ss");
+       String strDateTime = dateTime.format(cal.getTime());
+       return strDateTime;
+        
+    }
+    
     public String hello(String name) throws UnknownHostException {
        return String.format("Hello, %s from %s/%s", name, 
                             InetAddress.getLocalHost().getHostName(),
